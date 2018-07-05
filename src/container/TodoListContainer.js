ecdd593017d00349a91e566c1a049d1e54c4a952
chiabi <chiabi88@gmail.com>
2018-07-05 12:11:50 +0900

    비동기 작업

--- src/container/TodoListContainer.js
+++ src/container/TodoListContainer.js
@@ -1,15 +1,37 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
 import TodoList from '../components/TodoList';
+import {
+  fetchTodosRequest,
+  fetchTodosSuccess,
+  fetchTodosFailure,
+} from '../ducks/todos';
 
 function mapStateToProps(state) {
   return {
-    todos: state.todos,
-  }
+    todos: state.todos.items,
+    loading: state.todos.loading,
+    errorMsg: state.todos.errorMsg,
+  };
 }
 
-function mapDispatchToProps(dispatch) {}
+function mapDispatchToProps(dispatch) {
+  return {
+    onMount: async function() {
+      dispatch(fetchTodosRequest());
+      try {
+        const res = await axios.get('https://aspiring-waiter.glitch.me/todos');
+        dispatch(fetchTodosSuccess(res.data));
+      } catch (e) {
+        dispatch(fetchTodosFailure(e.message));
+      }
+    },
+  };
+}
 
-export default connect( // 다시 그리는 처리까지 한다.
+export default connect(
+  // 다시 그리는 처리까지 한다.
   mapStateToProps,
   mapDispatchToProps
-)(TodoList)
+)(TodoList);
--- src/ducks/todos.js
+++ src/ducks/todos.js
@@ -1,5 +1,8 @@
 // 액션 타입
 const ADD_TODO = 'fds-redux-todo/todo/ADD_TODO';
+const FETCH_TODOS_REQUEST = 'fds-redux-todo/todo/FETCH_TODOS_REQUEST';
+const FETCH_TODOS_SUCCESS = 'fds-redux-todo/todo/FETCH_TODOS_SUCCESS';
+const FETCH_TODOS_FAILURE = 'fds-redux-todo/todo/FETCH_TODOS_FAILURE';
 let idCount = 1;
 
 // 액션 생산자
@@ -11,17 +14,68 @@ export function addTodo(body) {
 }
 
 // 리듀서
-export default function todos(state = [], action) {
+export function fetchTodosRequest() {
+  return {
+    type: FETCH_TODOS_REQUEST,
+  };
+}
+
+export function fetchTodosSuccess(todos) {
+  return {
+    type: FETCH_TODOS_SUCCESS,
+    todos,
+  };
+}
+
+export function fetchTodosFailure(errorMsg) {
+  return {
+    type: FETCH_TODOS_FAILURE,
+    errorMsg,
+  };
+}
+
+// state 설계하기
+// 관리해야 할 데이터
+// - 할 일 목록
+// - 로딩 여부
+// - 에러메시지
+
+const initialState = {
+  items: [],
+  loading: false,
+  errorMsg: null,
+};
+export default function todos(state = initialState, action) {
   switch (action.type) {
     case ADD_TODO:
-      return [
+      return {
         ...state,
-        {
-          id: idCount++,
-          body: action.body,
-          complete: false,
-        },
-      ];
+        items: [
+          ...state.items,
+          {
+            id: idCount++,
+            body: action.body,
+            complete: false,
+          },
+        ],
+      };
+    case FETCH_TODOS_REQUEST:
+      // 로딩인디케이터 켜주기
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_TODOS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        items: action.todos,
+      };
+    case FETCH_TODOS_FAILURE:
+      return {
+        ...state,
+        errorMsg: action.errorMsg,
+      };
     default:
       return state;
   }
